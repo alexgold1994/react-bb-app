76446f7919852f091f89c52229ecc52d910a18b3
Alex <alexgold1994@gmail.com>
2021-04-09 22:46:26 +0300

    refactoring spinner as component

--- src/components/itemList/itemList.js
+++ src/components/itemList/itemList.js
@@ -1,6 +1,8 @@
 import React, {Component} from 'react';
 import { ListGroup, ListGroupItem } from 'reactstrap';
 import styled from 'styled-components';
+import bbService from '../../services/bbService';
+import StyledSpinner from '../spinner/spinner'
 
 const ListGroupItemOne = styled(ListGroupItem)`
     cursor: pointer;
@@ -9,7 +11,29 @@ const ListGroupItemOne = styled(ListGroupItem)`
 
 export default class ItemList extends Component {
 
+    bbService = new bbService();
+
+    state = {
+        charList: null
+    }
+
+/*     componentDidMount() {
+        this.bbService.getAllCharacters()
+            .then((charList) => {
+                this.setState({
+                    charList
+                })
+            })
+    } */
+
     render() {
+
+        const {charList} = this.state;
+
+        if (!charList) {
+            return <StyledSpinner/>
+        }
+
         return (
             <ListGroup>
                 <ListGroupItemOne>
